1f0ac2d3c2702374dd605a1683f0698fa5266ad2
DESKTOP-3MEB8RP\katou <naru0306@ezweb.ne.jp>
2024-10-20 09:28:54 +0900

    add header

---
+++ src/component/layouts/Header.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import styles from './Header.module.scss'; // CSSモジュールのインポート
+
+export default function Header() {
+    return (
+        <>
+            <header className={styles.header}>
+                <div className={styles.header__content}>
+                    <div className={styles.header__inner}>
+                        <h1 className={styles.headerLogo}>
+                            <Link href="https://www.front-blog.com">
+                                Front Blog
+                            </Link>
+                        </h1>
+
+                        <div className={styles.drawer_bg}></div>
+                        <nav className={styles.headerNav}>
+                            <ul className={styles.headerNavList}>
+                                <li className={styles.headerNavListItem}>
+                                    <Link href="https://www.front-blog.com">
+                                        ホーム
+                                    </Link>
+                                </li>
+                                <li className={styles.headerNavListItem}>
+                                    <Link href="https://www.front-blog.com/about">
+                                        サイト概要
+                                    </Link>
+                                </li>
+                                <li className={styles.headerNavListItem}>
+                                    <Link href="https://www.front-blog.com/contact">
+                                        お問い合わせ
+                                    </Link>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <div className={styles.headerToggle}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="first-view">
+                    {/* Swiperの部分はクライアントサイドでレンダリングされるべき */}
+                </div>
+            </header>
+        </>
+    );
+}
